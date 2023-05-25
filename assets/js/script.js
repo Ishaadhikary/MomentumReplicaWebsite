b21fb9e85856371acc3267428bc0b866211fb896
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-05-25 20:52:57 +0545

    Adding main time to the landing page

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+function time() {
+  let currentTime = new Date();
+  let currentHour = currentTime.getHours();
+  let currentMin = currentTime.getMinutes();
+  
+ 
+  let timeView = prompt("Do you want 12 hr or 24 hr");
+  switch (timeView) {
+    case "12hr":
+      {
+        let period = currentHour > 12 ? "PM" : "AM";
+        currentHour = currentHour > 12 ? currentHour - 12 : currentHour;
+        currentHour = currentHour == 12 ? 00 : currentHour;
+        currentHour = currentHour<10?"0"+currentHour:currentMin;
+        currentMin = currentMin<10?"0"+currentMin:currentMin;
+        const timeDisplay = document.createTextNode(currentHour + ":" + currentMin + " " + period);
+        let centerCenter = document.querySelector('.center-center');
+        centerCenter.appendChild(timeDisplay);
+      }
+      break;
+    case "24hr": {
+        currentHour = currentHour == 12 ? 00 : currentHour;
+        currentHour = currentHour<10?"0"+currentHour:currentMin;
+        currentMin = currentMin<10?"0"+currentMin:currentMin;
+        let timeDisplay = document.createTextNode(currentHour + ":" + currentMin);
+        let centerCenter = document.querySelector('.center-center');
+        centerCenter.appendChild(timeDisplay);
+    }
+  }
+}
+
+time();
