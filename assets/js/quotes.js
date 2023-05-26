4b686e2971db573aaaa08772c8c30767678de9fd
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-05-26 20:24:38 +0545

    Adding the quote and authors name to the website

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -1,18 +1,22 @@
-class Quotes extends Displaytime{
-    constructor(){
-        super()
-        this.quotesList =['"Give every day the chance to be the most beautiful day of your life."','"Nobody can go back and start a new beginning, but anyone can start today and make a new ending."','"The day will be what you make it, so rise, like the sun, and burn."','“Be pleasant until 10 o’clock in the morning, and the rest of the day will take care of itself.”','“In one minute you can change your attitude, and in that minute you can change your entire day.”','"Ruthlessly protect your time."','"The greatest productivity hack is the willingness to turn people down."','"If you did 4 hours of focused work every day you’d be ahead of 99% of people who multitask for 8 hours."']
-        this.authorList = ['Mark Twain','Maria Robinson','William C. Hannan','Elbert Hubbard','Spencer Johnson','Anthony Pompliano','Mark Manson','Dan Go']
-    }
-    changeQuote(){
-        if(currentHour>4){
-           const randomIndex = Math.floor(Math.random()*this.quotesList.length)
-           const quoteShowingId = document.querySelector('.bottom-center') 
-           const quoteShowing = this.quotesList[randomIndex]
-           quoteShowingId.textContent=quoteShowing;
-        }
-    }
+
+const auth = document.createElement("div")
+auth.style.textAlign='center';
+updateQuotes()
+setInterval(updateQuotes,10000) 
+function updateQuotes(){
+    
+        let randomIndex = Math.floor(Math.random()*quotesList.length)
+        const quoteShowingId = document.querySelector('.bottom-center') ;
+        let authorShowing = authorList[randomIndex]//to get the authors name
+        const quoteShowing = quotesList[randomIndex];//to get the quote
+        quoteShowingId.textContent=quoteShowing;//adding the quote to the webpage
+        auth.textContent="-" + authorShowing
+        quoteShowingId.addEventListener('mouseenter', e => { 
+            quoteShowingId.append(auth)//adding the author to the page when the pointer is set on the quote
+        })
+        console.log(authorShowing)
+        quoteShowingId.addEventListener('mouseleave', e => {quoteShowingId.removeChild(auth)}) //removing the authors name when the pointer is removed    
 }
 
-const quoteItem = new Quotes
-console.log(quoteItem.changeQuote());
+
+
