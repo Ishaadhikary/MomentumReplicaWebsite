0aad767205818f2a947984e858f591e546a9b0da
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-05-27 10:01:04 +0545

    Adding the functionality of the time display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,51 @@
-class Displaytime{
-  constructor(){
-  }
+class Displaytime {
+  constructor() {}
 
-updatetime() {
-  switch (timeView) {
-
-    case "24hr": {
-      const timeDisplay =documet.
-      timeDisplay = document.createTextNode( currentHour + ":" + currentMin);
-      let centerCenter = document.querySelector(".center-center");
-      centerCenter.appendChild(timeDisplay);
-      console.log(timeDisplay)
+  updatetime() {
+    let centerCenter = document.querySelector(".center-center");
+  
+    centerCenter.innerHTML = "";
+    switch (timeView) {
+      case "24hr":
+        {
+          let timeDisplay = document.createTextNode(
+            currentHour + ":" + currentMin
+          );
+          centerCenter.appendChild(timeDisplay);
+        }
+        break;
+      case "12hr":
+        {
+          currentHour =
+            currentHour > amPmCheck ? currentHour - 12 : currentHour;
+          const timeDisplay = document.createTextNode(
+            currentHour + ":" + currentMin + " " + period
+          );
+          centerCenter.append(timeDisplay);
+        }
+        break;
     }
-    break;
-    case "12hr":
-      {
-        currentHour = currentHour > amPmCheck ?  currentHour - 12 :  currentHour;
-        const timeDisplay = document.createTextNode(
-        currentHour + ":" +  currentMin + " " + period
-        );
-        let centerCenter = document.querySelector(".center-center");
-        console.log(timeDisplay)
-        centerCenter.append(timeDisplay);
-        console.log(timeDisplay)
-      }
-      break;
   }
 }
-}
 
-const time = new Displaytime()
-time.updatetime()
+const time = new Displaytime();
+time.updatetime();
 // For toggle bar
-function popBox(popElementId,parentElementId) {
-  const parentElement = document.getElementById(parentElementId)
+function popBox(popElementId, parentElementId) {
+  const parentElement = document.getElementById(parentElementId);
   const popUpElement = document.getElementById(popElementId);
   // console.log('child:'+ popUpElement);
   popUpElement.classList.toggle("show");
-  popUpElement.addEventListener("click",function(){
-    timeView ="12hr"
-    console.log(timeView)
-    time.updatetime()
-  })
+  popUpElement.addEventListener("click", function () {
+    if(timeView==='24hr'){
+    timeView = "12hr";
+    console.log(timeView);
+    time.updatetime();}
+    else{
+      timeView = "24hr";
+    console.log(timeView);
+    time.updatetime();
+    }
+  });
   // else console.log("hello")
 }
-  
-  
