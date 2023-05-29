51db190405e90d0da40135fe2d865cd6d5ced7a8
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-05-29 12:48:15 +0545

    Adding the info from local storage for the main task

--- assets/js/mainTask.js
+++ assets/js/mainTask.js
@@ -1,10 +1,14 @@
 let mainTaskCheckId = document.getElementById("mainTaskCheckId");
 let mainTaskTextId = document.getElementById("mainTaskTextId");
+let inputTask= document.querySelector(".inputTask")
 console.log("UserTask:", mainTaskTextId)
 function mainTask(){
     mainTaskTextId.addEventListener("keydown",function(e){
         if(e.key ==="Enter" ){
             localStorage.setItem("mainTaskText",mainTaskTextId.value)
+            storageMainTask= localStorage.getItem("mainTaskText")
+            inputTask.textContent=storageMainTask
+
         }
     })
 }
