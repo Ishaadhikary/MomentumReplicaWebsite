27c44923147c2b973e2e7fa9abff07f9b6dfff84
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-05-31 17:50:35 +0545

    Adding css to the brainDump

--- assets/js/braindump.js
+++ assets/js/braindump.js
@@ -3,13 +3,13 @@ let brainDumpContainer = document.querySelector(".brainDumpContainer");
 let bulbId = document.getElementById("bulb");
 let brainDumpIdeas = document.querySelector(".brainDumpIdeas");
 
-
 let countB = 1;
 function bulb() {
   if (countB % 2 != 0) {
     bulbId.src = "assets/images/bulbOn.svg";
     brainDumpContainer.style.display = "flex";
     countB++;
+    addIdeas()
     //event Listner on clicking add option go
   } else {
     bulbId.src = "assets/images/bulbOff.svg";
@@ -19,21 +19,23 @@ function bulb() {
   }
 }
 function addIdeas() {
-  let newIdeas = brainDumpInput.value
-  if(localStorage.getItem("ideas")==null){
-    localStorage.setItem("ideas",'[]')
+  let newIdeas = brainDumpInput.value;
+  if (localStorage.getItem("ideas") == null) {
+    localStorage.setItem("ideas", "[]");
+
   }
-  let oldIdeas = JSON.parse(localStorage.getItem("ideas"))
-  oldIdeas.unshift(newIdeas)
+  let oldIdeas = JSON.parse(localStorage.getItem("ideas"));
+  oldIdeas.unshift(newIdeas);
   localStorage.setItem("ideas", JSON.stringify(oldIdeas));
   let storedIdeas = JSON.parse(localStorage.getItem("ideas"));
-  brainDumpIdeas.innerHTML=""
-  // ideas.push(brainDumpInput.value);
-  storedIdeas.forEach(storedIdea => {
-    
-    let li = document.createElement("li")
-    li.innerHTML = storedIdea
-    brainDumpIdeas.append(li)
-    
- });
+  brainDumpIdeas.innerHTML = "";
+  storedIdeas.forEach((storedIdea) => {
+    let li = document.createElement("li");
+    li.innerHTML = storedIdea;
+    if(li.innerHTML !=""){
+
+      brainDumpIdeas.append(li);
+    }
+  
+  });
 }
