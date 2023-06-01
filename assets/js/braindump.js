082a48b831d03e8f7f2e7f993bee03fa5e8cf7e8
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-06-01 17:19:27 +0545

    Creating new array with edited information

--- assets/js/braindump.js
+++ assets/js/braindump.js
@@ -3,7 +3,7 @@ let brainDumpContainer = document.querySelector(".brainDumpContainer");
 let bulbId = document.getElementById("bulb");
 let brainDumpIdeas = document.querySelector(".brainDumpIdeas");
 let editIdeasButton = document.querySelector(".editIdeas");
-let addIdeasButton = document.querySelector(".addIdeas")
+let addIdeasButton = document.querySelector(".addIdeas");
 
 let countB = 1;
 function bulb() {
@@ -11,8 +11,8 @@ function bulb() {
     bulbId.src = "assets/images/bulbOn.svg";
     brainDumpContainer.style.display = "flex";
     countB++;
-    editIdeasButton.style.display= "none"
-    addIdeas()
+    editIdeasButton.style.display = "none";
+    addIdeas();
     //event Listner on clicking add option go
   } else {
     bulbId.src = "assets/images/bulbOff.svg";
@@ -25,67 +25,60 @@ function addIdeas() {
   let newIdeas = brainDumpInput.value;
   if (localStorage.getItem("ideas") == null) {
     localStorage.setItem("ideas", "[]");
-    console.log("check")
+    console.log("check");
   }
   var oldIdeas = JSON.parse(localStorage.getItem("ideas"));
-  if(newIdeas != ""){
-  oldIdeas.unshift(newIdeas);
- }
- localStorage.setItem("ideas", JSON.stringify(oldIdeas));
- let storedIdeas = JSON.parse(localStorage.getItem("ideas"));
+  if (newIdeas != "") {
+    oldIdeas.unshift(newIdeas);
+  }
+  localStorage.setItem("ideas", JSON.stringify(oldIdeas));
+  let storedIdeas = JSON.parse(localStorage.getItem("ideas"));
   brainDumpIdeas.innerHTML = "";
-  storedIdeas.forEach((storedIdea,editIndex) => {
-    let eachIdeaContainer = document.createElement("div")
-    eachIdeaContainer.id = "eachIdeaContainer"
-    let editIcon = document.createElement("img")
-    editIcon.src="assets/images/edit.svg"
-    editIcon.id ="editIcon"
-    let deleteIcon = document.createElement("img")
-    deleteIcon.src="assets/images/delete.svg"
-    deleteIcon.id = "deleteIcon"
+  storedIdeas.forEach((storedIdea, editIndex) => {
+    let eachIdeaContainer = document.createElement("div");
+    eachIdeaContainer.id = "eachIdeaContainer";
+    let editIcon = document.createElement("img");
+    editIcon.src = "assets/images/edit.svg";
+    editIcon.id = "editIcon";
+    let deleteIcon = document.createElement("img");
+    deleteIcon.src = "assets/images/delete.svg";
+    deleteIcon.id = "deleteIcon";
     let li = document.createElement("li");
     li.innerHTML = storedIdea;
-    if(li.innerHTML !=""){
-      eachIdeaContainer.append(li,editIcon,deleteIcon)
-      brainDumpIdeas.append(eachIdeaContainer);
-    }
-    if(li.innerHTML=="" && localStorage.getItem("ideas") == null){
-      localStorage.removeItem("ideas", "")
-    }
-    editIndex++
+
+    eachIdeaContainer.append(li, editIcon, deleteIcon);
+    brainDumpIdeas.append(eachIdeaContainer);
+
+    editIndex++;
     editIcon.onclick = () => editIdeas(editIndex);
   });
 }
-  
-  // console.log(index)
 
+// console.log(index)
 
 //edit each of the Ideas
-function editIdeas(editIndex){
-  editIdeasButton.style.display= "inline-block"
-  console.log(editIndex)
+function editIdeas(editIndex) {
+  editIdeasButton.style.display = "inline-block";
+  console.log(editIndex);
   let oldIdea = JSON.parse(localStorage.getItem("ideas"));
-  let editValue = oldIdea.slice(editIndex-1,editIndex);
-  brainDumpInput.value=editValue
-  console.log(addIdeasButton)
-  addIdeasButton.style.display="none"
-  editIdeasButton.addEventListener("click",function(){console.log("Editing old Ideas")
-  let oldIdeas = JSON.parse(localStorage.getItem("ideas"));
-  console.log(oldIdeas)
-  let oldIdea1= oldIdeas.splice(editIndex,oldIdeas.length)
-  let oldIdea2 =oldIdeas.splice(0,editIndex-1 )
-  let c=oldIdea1.concat(oldIdea2)
-  console.log(c)
-})
+  let editValue = oldIdea.slice(editIndex - 1, editIndex);
+  brainDumpInput.value = editValue;
+  console.log(addIdeasButton);
+  addIdeasButton.style.display = "none";
+  editIdeasButton.addEventListener("click", function () {
+    console.log("Editing old Ideas");
+    let oldIdeas = JSON.parse(localStorage.getItem("ideas"));
+    console.log(oldIdeas);
+    oldIdeas.splice(editIndex-1,1)
+    console.log(oldIdeas);
+  });
 }
 // function editOldIdeas(){
 //   console.log("Editing old Ideas")
 //   let oldIdeas = JSON.parse(localStorage.getItem("ideas"));
 //   oldIdeas.splice(editIndex)
-  
-  
+
 // }
-// console.log("Old Ideas"+oldIdeas[index]) 
+// console.log("Old Ideas"+oldIdeas[index])
 // brainDumpInput.value=oldIdeas[index]
 // console.log(brainDumpInput.value)
-
