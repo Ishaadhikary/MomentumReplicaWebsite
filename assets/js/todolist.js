70dd5e30f423ac3a0ac6a9816b308bcae240f7fb
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-06-02 08:21:27 +0545

    Updating the display for collapse and expandable todo list container

--- assets/js/todolist.js
+++ assets/js/todolist.js
@@ -1,10 +1,23 @@
 let todoListContainer = document.querySelector(".todoListContainer")
 let expand = document.getElementById("expand")
 let countTodo=1
+let todoTable= document.getElementById("todoTable")
+let headerRow = document.querySelector("#todoTable tr")
+console.log(headerRow)
+let statusColumn = document.getElementById("statusColumn")
+let taskColumn = document.getElementById("taskColumn")
+let categoryColumn = document.getElementById("categoryColumn")
+let timerColumn = document.getElementById("timerColumn")
+let priorityColumn = document.getElementById("priorityColumn")
+
 function todoList(){
     if(countTodo % 2 != 0)
     {
     todoListContainer.style.display ="block"
+    categoryColumn.style.display="none"
+    timerColumn.style.display="none"
+    priorityColumn.style.display="none"
+    
     countTodo++
     }
     else{
@@ -19,11 +32,20 @@ expand.addEventListener("click", function(){
     todoListContainer.style.width="600px"
     todoListContainer.style.height="600px"
     expand.src="assets/images/collapse.svg"
+    taskColumn.style.width="50%"
+    categoryColumn.style.display="table-cell"
+    timerColumn.style.display="table-cell"
+    priorityColumn.style.display="table-cell"
     countE++
 }
     else {
     todoListContainer.style.width="400px"
     todoListContainer.style.height="300px"
+    taskColumn.style.width="90%"
+    categoryColumn.style.display="none"
+    timerColumn.style.display="none"
+    priorityColumn.style.display="none"
+    
     expand.src="assets/images/expand.svg"
     countE++
      }
