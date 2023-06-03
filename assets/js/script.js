eb38f60a611e1dc1e7a2cbfae8112106f64007e6
Ishaadhikary <isha.adhikary12354@gmail.com>
2023-06-03 17:30:01 +0545

    Adding pomodoro timer feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,9 @@ let centerCenter = document.querySelector(".center-center");
 let centerTime = document.querySelector(".center-time");
 let centerPercentage = document.querySelector(".center-percentage");
 let centerPomodoro = document.querySelector(".center-pomodoro");
-let click = new Audio("../assets/audio/click.mp3")
-let alarm = new Audio("../assets/audio/alarm.mp3")
+let click = new Audio("../assets/audio/click.mp3");
+let alarm = new Audio("../assets/audio/alarm.mp3");
+
 
 updateTime();
 setInterval(updateTime, 1000);
@@ -105,6 +106,7 @@ function pomodoroDisplay() {
   let pomodoroTopRight = document.createElement("button")
   let pomodoroBottom = document.createElement("button")
   let pomodoroBottomShortBreak = document.createElement("button")
+  let pomodoroBottomLongBreak = document.createElement("button")
   pomodoroTopContainer.id = "pomodoroTopContainer"
   pomodoroTopLeft.id = "pomodoroTopLeft"
   pomodoroTopRight.id = "pomodoroTopRight"
@@ -112,23 +114,27 @@ function pomodoroDisplay() {
   pomodoroCenterLeft.id = "pomodoroCenterLeft"
   pomodoroCenterRight.id= "pomodoroCenterRight"
   pomodoroBottomShortBreak.id ="pomodoroBottomShortBreak"
+  pomodoroBottomLongBreak.id = "pomodoroBottomLongBreak"
+pomodoroCenterContainer.id = "pomodoroCenterContainer" 
+pomodoroBottomContainer.id = "pomodoroBottomContainer"
   pomodoroTopLeft.innerText="Pomodoro"
   pomodoroTopRight.innerText="Break"
   pomodoroBottomShortBreak.innerHTML="BREAK"
+  pomodoroBottomLongBreak.innerHTML="LONG BREAK"
   pomodoroBottom.innerText = "START"
   pomodoroCenterLeft.innerHTML="24 : "
   pomodoroCenterRight.innerHTML=" 59"
-pomodoroCenterContainer.id = "pomodoroCenterContainer" 
-pomodoroBottomContainer.id = "pomodoroBottomContainer"
 pomodoroCenterContainer.append(pomodoroCenterLeft,pomodoroCenterRight)
-pomodoroBottomContainer.append(pomodoroBottom,pomodoroBottomShortBreak)
+pomodoroBottomContainer.append(pomodoroBottom,pomodoroBottomShortBreak,pomodoroBottomLongBreak)
 pomodoroTopContainer.append(pomodoroTopLeft,pomodoroTopRight)
 pomodoroContainer.append(pomodoroTopContainer,pomodoroCenterContainer,pomodoroBottomContainer)
 centerPomodoro.append(pomodoroContainer) 
 pomodoroTopLeft.style.backgroundColor="#424242"
 pomodoroBottomShortBreak.style.display="none"
+pomodoroBottomLongBreak.style.display="none"
 pomodoroBottomShortBreak.addEventListener("click", shortBreakTimer)
-pomodoroBottom.addEventListener("click", pomodoroTimer)    
+pomodoroBottom.addEventListener("click", pomodoroTimer)   
+pomodoroBottomLongBreak.addEventListener("click",longBreakTimer) 
 }
 let minIntervalId;
 let secIntervalId;
@@ -147,14 +153,21 @@ function pomodoroTimer(){
     pomodoroCenterRight.innerHTML = "59"
     alarm.play() 
     pomodoroBottom.style.display="none"
+    pomodoroBottomLongBreak.style.display="none"
     pomodoroBottomShortBreak.style.display="inline-block" 
-    count++
-    console.log(count,'count')
-}, 120000);//This is ending the timer in one min: change it to 25 min for demo
+    console.log(countP,'count');
+    countP++
+    if (countP %4 ==0){
+      pomodoroBottom.style.display="none"
+      pomodoroBottomShortBreak.style.display="none"
+      pomodoroBottomLongBreak.style.display="inline-block"
+      pomodoroCenterLeft.innerHTML="15:"
+      pomodoroCenterRight.innerHTML="59"
+    }
+}, 1500000);
+//This is ending the timer in one min: change it to 25 min for demo
    //To check if the pomodoroTimer has been executed 4 times
-  if (count ==4){
-    console.log("Take long break")
-  }
+ 
 
 }
 let pomoMin =24
@@ -165,8 +178,7 @@ function pomoMinChange(){
   if(pomoMin>=0)
   {
     pomoMin--
-    pomodoroCenterLeft.innerHTML= (pomoMin<10)?"0"+pomoMin+ " :" : pomoMin + " :"
-    
+    pomodoroCenterLeft.innerHTML= (pomoMin<10)?"0"+pomoMin+ " :" : pomoMin + " :" 
     
   }
 }
@@ -194,31 +206,54 @@ function shortBreakTimer(){
   setTimeout(function(){
     clearInterval(minIntervalId);
     clearInterval(secIntervalId)
-    pomodoroTopLeft.style.background="none";
-    pomodoroTopRight.style.backgroundColor="#424242"
-    pomodoroCenterLeft.innerHTML= "25:"
+    pomodoroTopLeft.style.background="#424242";
+    pomodoroTopRight.style.background="none"
+    pomodoroCenterLeft.innerHTML= "24:"
     pomodoroCenterRight.innerHTML = "00"
     alarm.play() 
     pomodoroBottom.style.display="inline-block"
-    pomodoroBottomShortBreak.style.display="none" 
-  pomodoroBottom.addEventListener("click", pomodoroTimer)
-    count++
+    pomodoroBottomShortBreak.style.display="none"
+    pomodoroBottomLongBreak.style.display="none" 
 }, 300000);
 
 }
 let shortMin = 4
 //function to change minutes for pomodoro
 function shortMinChange(){
-  count ++
   let pomodoroCenterLeft = document.querySelector("#pomodoroCenterLeft")
   if(shortMin>=0)
   {
     shortMin--
-    pomodoroCenterLeft.innerHTML= "0 : " + shortMin
+    pomodoroCenterLeft.innerHTML= "0" + shortMin+ " :"
 
   }
-
 }
 
+function longBreakTimer(){
+  click.play()
+ 
+  minIntervalId = setInterval(longMinChange,60000)
+  secIntervalId= setInterval(secChange,1000)
+  setTimeout(function(){
+    clearInterval(minIntervalId);
+    clearInterval(secIntervalId)
+    pomodoroCenterLeft.innerHTML="24:"
+    pomodoroCenterRight.innerHTML="59"
+    pomodoroTopLeft.style.background="#424242";
+    pomodoroTopRight.style.background="none"
+    alarm.play() 
+    pomodoroBottom.style.display="inline-block"
+    pomodoroBottomShortBreak.style.display="none"
+    pomodoroBottomLongBreak.style.display="none" 
+}, 900000);
+}
+let longMin =14
+function longMinChange(){
+  if(longMin>=0)
+  {
+    longMin--
+    pomodoroCenterLeft.innerHTML= (longMin<10)?"0" + longMin+ " :" : longMin + " :"
 
+  }
+}
 
